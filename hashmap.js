f09e64871ddd263d9e344f8557c80e49da8ab482
TipDevs <tipdevs@gmail.com>
2025-08-27 19:08:54 +0100

    Add an hash code generator method to the HashMap class function

--- hashmap.js
+++ hashmap.js
@@ -4,6 +4,16 @@ class HashMap {
     this.loadFactor = 0.7;
     this.buckets = Array.from({ length: this.capacity }, () => []);
   }
+  hash(key) {
+    // takes a key and produces a hash code with it
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode %= this.capacity;
+    }
+    return hashCode;
+  }
 }
 
 const test = new HashMap();

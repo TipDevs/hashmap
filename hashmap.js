cf31a327cdc595bab438c0a30a9ddbaa1ab75a56
TipDevs <tipdevs@gmail.com>
2025-08-27 19:12:04 +0100

    Add a set method that takes in two arguments and stored them in a bucket if empty or update the value if not empty

--- hashmap.js
+++ hashmap.js
@@ -14,6 +14,20 @@ class HashMap {
     }
     return hashCode;
   }
+  set(key, value) {
+    // takes two arguments:
+    // the first is a key, and
+    // the second is a value that is assigned to this key.
+    let index = this.hash(key);
+    for (let bucket of this.buckets[index]) {
+      // If a key already exists, then the old value is overwritten
+      if (bucket[0] === key) {
+        bucket[1] = value;
+        return;
+      }
+    }
+    this.buckets[index].push([key, value]);
+  }
 }
 
 const test = new HashMap();
